dad5644030be495a041a02770ed8536e717e00e6
akmalviya03 <abhishakkrmalviya@gmail.com>
2022-09-09 15:25:16 +0530

    FrictionSimulation

---
+++ android/app/src/main/kotlin/com/example/friction_simulation_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.friction_simulation_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: FrictionSimulationDemo(),
+    );
+  }
+}
+
+class FrictionSimulationDemo extends StatefulWidget {
+  const FrictionSimulationDemo({Key? key}) : super(key: key);
+
+  @override
+  State<FrictionSimulationDemo> createState() => _FrictionSimulationDemoState();
+}
+
+class _FrictionSimulationDemoState extends State<FrictionSimulationDemo>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Alignment> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController.unbounded(vsync: this);
+    _animation = _controller.drive(
+      AlignmentTween(
+        begin: const Alignment(
+          -1,
+          0,
+        ),
+        end: const Alignment(
+          1,
+          0,
+        ),
+      ),
+    );
+
+    _controller.addListener(() {
+      if (kDebugMode) {
+        print("Controller ${_controller.value}");
+        print("Animation ${_animation.value}");
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnimatedBuilder(
+                        animation: _animation,
+                        child: Container(
+                          color: Colors.black,
+                          height: 10,
+                          width: 10,
+                        ),
+                        builder: (context, child) {
+                          return Align(
+                            alignment: _animation.value,
+                            child: child,
+                          );
+                        }),
+                    Container(
+                      width: double.maxFinite,
+                      height: 10,
+                      color: Colors.green,
+                    )
+                  ],
+                ),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    _controller.animateWith(FrictionSimulation(
+                      0.4,
+                      0,
+                      1,
+                    ));
+                  },
+                  child: const Text('Play Friction Simulation')),
+              ElevatedButton(
+                  onPressed: () {
+                    _controller.stop();
+                  },
+                  child: const Text('Stop Simulation'))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
